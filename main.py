74516634c707262c2ae5303f2864d00553914534
shynnee <nguyentrongphuocbh.912@gmail.com>
2024-05-25 01:38:57 +0700

    update

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import numpy as np
 import tkinter as tk
 from tkinter import messagebox
 from pathlib import Path
-import glob
 import threading
 import subprocess
 import sqlite3
@@ -19,7 +18,7 @@ class FaceUnlockSystem:
                 self.original_labels = pickle.load(self.label_file)
             print(self.original_labels)
         except FileNotFoundError:
-            print("No user_labels.pickle file detected, will create required pickle files")
+            print("Không tìm thấy tệp user_labels.pickle, sẽ tạo các tệp pickle cần thiết")
 
         self.current_user_id = 0
         self.user_labels_ids = {}
@@ -52,7 +51,7 @@ class FaceUnlockSystem:
                     self.image_encoding = face_recognition.face_encodings(self.image)[0]
                     self.known_face_encodings.append([self.label, self.image_encoding])
             print(self.known_face_encodings)
-            print("Total Number of Images: " + str(len(self.known_face_encodings)))
+            print("Tổng số lượng hình ảnh: " + str(len(self.known_face_encodings)))
             with open('KnownFaces.pickle', 'wb') as self.known_faces_file:
                 pickle.dump(self.known_face_encodings, self.known_faces_file)
         else:
@@ -80,13 +79,13 @@ class FaceUnlockSystem:
                         self.face_distances = face_recognition.face_distance([self.known_face[1]], self.face_encoding)
                         self.best_match_index = np.argmin(self.face_distances)
                         print(self.best_match_index)
-                        print('Match found:', self.matches[self.best_match_index])
+                        print('Trùng khớp:', self.matches[self.best_match_index])
                         if self.matches[self.best_match_index]:
                             self.continue_running = False
                             self.detected_user_names.append(self.known_face[0])
                             break
                         next
-            print("Best match(es): " + str(self.detected_user_names))
+            print("Kết quả trùng khớp tốt nhất: " + str(self.detected_user_names))
             self.video_capture.release()
             cv2.destroyAllWindows()
             break
@@ -95,7 +94,7 @@ class FaceUnlockSystem:
 
 def register_user():
     if not (full_name_var.get() and username_var.get() and password_var.get()):
-        messagebox.showerror("Error", "All fields are required!")
+        messagebox.showerror("Lỗi", "Tất cả các trường đều là bắt buộc!")
         return
 
     if not os.path.exists("user_images"):
@@ -109,24 +108,24 @@ def register_user():
     number_of_files += 1
 
     cam = cv2.VideoCapture(0)
-    cv2.namedWindow("Capture Image")
+    cv2.namedWindow("Chụp Ảnh")
 
     while True:
         ret, frame = cam.read()
-        cv2.imshow("Capture Image", frame)
+        cv2.imshow("Chụp Ảnh", frame)
         if not ret:
             break
         key_press = cv2.waitKey(1)
 
         if key_press % 256 == 27:
-            print("Escape hit, closing...")
+            print("Nhấn Escape, đóng cửa sổ...")
             cam.release()
             cv2.destroyAllWindows()
             return
         elif key_press % 256 == 32:
             img_name = os.path.join(user_image_dir, f"{number_of_files}.png")
             cv2.imwrite(img_name, frame)
-            print(f"{img_name} saved!")
+            print(f"{img_name} đã được lưu!")
             cam.release()
             cv2.destroyAllWindows()
             break
@@ -149,18 +148,18 @@ def register_user():
         conn.close()
 
     except sqlite3.IntegrityError:
-        messagebox.showerror("Error", "Username already exists!")
+        messagebox.showerror("Lỗi", "Tên người dùng đã tồn tại!")
         return
 
     raise_frame(login_frame)
 
 
 def login_user():
-    print("Login button clicked")
+    print("Nút đăng nhập được nhấn")
     face_unlock_system = FaceUnlockSystem()
     user = face_unlock_system.identify_user()
     if not user:
-        messagebox.showerror("Alert", "Face Not Recognised")
+        messagebox.showerror("Cảnh báo", "Không nhận diện được khuôn mặt")
         return
 
     username = user[0]
@@ -189,31 +188,69 @@ def login_user():
     run_additional_threads()
 
 
+def login_with_credentials():
+    if not (username_login_var.get() and password_login_var.get()):
+        messagebox.showerror("Lỗi", "Tất cả các trường đều là bắt buộc!")
+        return
+
+    conn = sqlite3.connect('face_recognition.db')
+    c = conn.cursor()
+
+    c.execute('SELECT * FROM users WHERE username = ? AND password = ?', (username_login_var.get(), password_login_var.get()))
+    result = c.fetchone()
+
+    if not result:
+        messagebox.showerror("Lỗi", "Tên người dùng hoặc mật khẩu không đúng!")
+        return
+
+    username = username_login_var.get()
+    logged_in_user.set(username)
+
+    c.execute('SELECT login_count FROM login_attempts WHERE username = ?', (username,))
+    result = c.fetchone()
+
+    login_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+    if result:
+        login_count = result[0] + 1
+        c.execute('UPDATE login_attempts SET login_count = ?, last_login_time = ? WHERE username = ?',
+                  (login_count, login_time, username))
+    else:
+        c.execute('INSERT INTO login_attempts (username, login_count, last_login_time) VALUES (?, 1, ?)',
+                  (username, login_time))
+
+    conn.commit()
+    conn.close()
+
+    user_menu_frame.destroy()
+    run_additional_threads()
+
+
 def run_additional_threads():
-    def run_window():
-        subprocess.run(["python", "window.py"])
+    def run_subprocess():
+        subprocess.run(['python', 'window.py'])
 
-    def run_game():
-        subprocess.run(["python", "newgame/game.py"])
+    def create_window():
+        subprocess.run(['python', 'newgame/game.py'])
 
-    window_thread = threading.Thread(target=run_window)
-    game_thread = threading.Thread(target=run_game)
+    window_thread = threading.Thread(target=create_window)
+    game_thread = threading.Thread(target=run_subprocess)
 
     window_thread.start()
     game_thread.start()
 
-    window_thread.join()
-    game_thread.join()
-
 
 app = tk.Tk()
-app.title("Face Login System")
+app.geometry("1200x800")  # Tăng kích thước cửa sổ
+app.title("Hệ Thống Đăng Nhập Bằng Khuôn Mặt")
 
 login_frame = tk.Frame(app)
 registration_frame = tk.Frame(app)
 user_menu_frame = tk.Frame(app)
+login_options_frame = tk.Frame(app)
+credentials_login_frame = tk.Frame(app)
 
-frame_list = [login_frame, registration_frame, user_menu_frame]
+frame_list = [login_frame, registration_frame, user_menu_frame, login_options_frame, credentials_login_frame]
 for frame in frame_list:
     frame.grid(row=0, column=0, sticky='news')
     frame.configure(bg='white')
@@ -227,30 +264,51 @@ def raise_registration_frame():
 def raise_login_frame():
     raise_frame(login_frame)
 
+def raise_login_options_frame():
+    raise_frame(login_options_frame)
+
+def raise_credentials_login_frame():
+    raise_frame(credentials_login_frame)
+
 full_name_var = tk.StringVar()
 username_var = tk.StringVar()
 password_var = tk.StringVar()
+username_login_var = tk.StringVar()
+password_login_var = tk.StringVar()
 logged_in_user = tk.StringVar()
 
-tk.Label(login_frame, text="Face Recognition", font=("Courier", 60), bg="white").grid(row=1, column=1, columnspan=5)
-tk.Button(login_frame, text="Login", bg="white", font=("Arial", 30), command=login_user).grid(row=2, column=5)
-tk.Button(login_frame, text="Register", command=raise_registration_frame, bg="white", font=("Arial", 30)).grid(row=2, column=1)
+tk.Label(login_frame, text="Nhận Diện Khuôn Mặt", font=("Courier", 60), bg="white").grid(row=1, column=1, columnspan=5)
+tk.Button(login_frame, text="Đăng Nhập", bg="white", font=("Arial", 30), command=raise_login_options_frame).grid(row=2, column=5)
+tk.Button(login_frame, text="Đăng Ký", command=raise_registration_frame, bg="white", font=("Arial", 30)).grid(row=2, column=1)
 
-tk.Label(registration_frame, text="Register", font=("Courier", 60), bg="white").grid(row=1, column=1, columnspan=5)
-tk.Label(registration_frame, text="Full Name: ", font=("Arial", 30), bg="white").grid(row=2, column=1)
+tk.Label(registration_frame, text="Đăng Ký", font=("Courier", 60), bg="white").grid(row=1, column=1, columnspan=5)
+tk.Label(registration_frame, text="Họ Tên: ", font=("Arial", 30), bg="white").grid(row=2, column=1)
 tk.Entry(registration_frame, textvariable=full_name_var, font=("Arial", 30)).grid(row=2, column=2)
 
-tk.Label(registration_frame, text="Username: ", font=("Arial", 30), bg="white").grid(row=3, column=1)
+tk.Label(registration_frame, text="Tên Đăng Nhập: ", font=("Arial", 30), bg="white").grid(row=3, column=1)
 tk.Entry(registration_frame, textvariable=username_var, font=("Arial", 30)).grid(row=3, column=2)
 
-tk.Label(registration_frame, text="Password: ", font=("Arial", 30), bg="white").grid(row=4, column=1)
+tk.Label(registration_frame, text="Mật Khẩu: ", font=("Arial", 30), bg="white").grid(row=4, column=1)
 tk.Entry(registration_frame, textvariable=password_var, font=("Arial", 30), show='*').grid(row=4, column=2)
 
-tk.Button(registration_frame, text="Register", command=register_user, bg="white", font=("Arial", 30)).grid(row=5, column=2)
+tk.Button(registration_frame, text="Đăng Ký", command=register_user, bg="white", font=("Arial", 30)).grid(row=5, column=2)
 
-tk.Label(user_menu_frame, text="Hello, ", font=("Courier", 60), bg="white").grid(row=1, column=1)
+tk.Label(user_menu_frame, text="Xin Chào, ", font=("Courier", 60), bg="white").grid(row=1, column=1)
 tk.Label(user_menu_frame, textvariable=logged_in_user, font=("Courier", 60), bg="white", fg="red").grid(row=1, column=2)
-tk.Button(user_menu_frame, text="Back", font=("Arial", 30), command=raise_login_frame).grid(row=2, column=1)
+tk.Button(user_menu_frame, text="Trở Lại", font=("Arial", 30), command=raise_login_frame).grid(row=2, column=1)
+
+tk.Label(login_options_frame, text="Tùy Chọn Đăng Nhập", font=("Courier", 60), bg="white").grid(row=1, column=1, columnspan=5)
+tk.Button(login_options_frame, text="Đăng Nhập Bằng Nhận Diện Khuôn Mặt", bg="white", font=("Arial", 30), command=login_user).grid(row=2, column=1)
+tk.Button(login_options_frame, text="Đăng Nhập Bằng Tên Đăng Nhập và Mật Khẩu", bg="white", font=("Arial", 30), command=raise_credentials_login_frame).grid(row=2, column=2)
+
+tk.Label(credentials_login_frame, text="Tên Đăng Nhập: ", font=("Arial", 30), bg="white").grid(row=1, column=1)
+tk.Entry(credentials_login_frame, textvariable=username_login_var, font=("Arial", 30)).grid(row=1, column=2)
+
+tk.Label(credentials_login_frame, text="Mật Khẩu: ", font=("Arial", 30), bg="white").grid(row=2, column=1)
+tk.Entry(credentials_login_frame, textvariable=password_login_var, font=("Arial", 30), show='*').grid(row=2, column=2)
+
+tk.Button(credentials_login_frame, text="Đăng Nhập", command=login_with_credentials, bg="white", font=("Arial", 30)).grid(row=3, column=2)
+tk.Button(credentials_login_frame, text="Trở Lại", command=raise_login_options_frame, bg="white", font=("Arial", 30)).grid(row=3, column=1)
 
 raise_frame(login_frame)
 app.mainloop()
